a5dae176332121b84837eb93337d01686e3d921c
pateluday07 <pateluday07@gmail.com>
2026-04-12 19:52:11 +0530

    use @mapsid

--- src/main/java/com/byteandbeyondwithuday/springbootpractical/entity/IdCard.java
+++ src/main/java/com/byteandbeyondwithuday/springbootpractical/entity/IdCard.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public class IdCard {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
     private Long id;
     @Column(nullable = false, unique = true, length = 50)
     private String cardNumber;
@@ -18,6 +18,7 @@ public class IdCard {
     private LocalDate issueDate;
     @Column(nullable = false)
     private LocalDate expiryDate;
+    @MapsId
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "employee_id", nullable = false, unique = true)
     @JsonIgnore
